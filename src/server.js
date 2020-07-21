36f612d72e5525e3110f410c11ad2837985cff1a
Gustolandia <63708303+Gustolandia@users.noreply.github.com>
2020-07-22 00:55:29 +0100

    server file

---
+++ src/server.js
@@ -0,0 +1,60 @@
+const express = require("express")
+const listEndpoints = require("express-list-endpoints")
+const studentsRoutes = require("./services/users")
+const projectsRoutes = require("./services/projects")
+const cors = require("cors")
+const path = require("path"); 
+const mongoose = require("mongoose")
+
+
+
+
+const {
+  notFoundHandler,
+  unauthorizedHandler,
+  forbiddenHandler,
+  catchAllHandler,
+} = require("./errorHandling")
+
+
+const server = express()
+
+const port = process.env.PORT || 3004
+
+server.use(cors())
+
+const loggerMiddleware = (req, res, next) => {
+  console.log(`Logged ${req.url} ${req.method} -- ${new Date()}`)
+  next()
+}
+
+const publicFolderPath = path.join(__dirname, "./public")
+
+server.use(express.static(publicFolderPath));
+server.use(express.json()); // parse the bodies when they are in json format
+
+server.use(loggerMiddleware);
+
+server.use("/students", studentsRoutes)
+server.use("/projects", projectsRoutes)
+
+
+server.use(notFoundHandler)
+server.use(unauthorizedHandler)
+server.use(forbiddenHandler)
+server.use(catchAllHandler)
+
+console.log(listEndpoints(server))
+
+mongoose
+  .connect("mongodb://localhost:27017/strive", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(
+    server.listen(port, () => {
+      console.log("Running on port", port)
+    })
+  )
+  .catch((err) => console.log(err))
