2a25de293801030765a492b32864aa9157ceddd0
Gustolandia <63708303+Gustolandia@users.noreply.github.com>
2020-07-23 22:47:01 +0100

    Update index.js
    Experience

--- src/services/profiles/index.js
+++ src/services/profiles/index.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const ProfileSchema = require("./schema")
+const ExperienceSchema = require("../Experience/schema")
 const q2m = require("query-to-mongo")
 var auth = require('basic-auth')
 const multer = require("multer")
@@ -22,7 +23,7 @@ router.get("/:username", async (req, res, next) => {
   try {
     
     const username = req.params.username
-    const profile = await ProfileSchema.findByusername(username)
+    const profile = await ProfileSchema.find({username:username})
     if (profile) {
       res.send(profile)
     } else {
@@ -35,6 +36,8 @@ router.get("/:username", async (req, res, next) => {
     next("While reading profiles list a problem occurred!")
   }
 })
+
+
 router.post("/:username/picture", upload.any("picture"), async (req, res, next) => {
   console.log("mama")
   console.log(req.files)
@@ -58,6 +61,9 @@ router.post("/:username/picture", upload.any("picture"), async (req, res, next) 
   }
   res.send("ok")
 })
+
+
+
 router.get("/me", async (req, res, next) => {
   try {
     
@@ -191,5 +197,98 @@ async (req, res, next) => {
       next(error);
     }
   })
+
+
+
+  router.get("/:username/experiences", async (req, res, next) => {
+    try {
+      
+      const username = req.params.username
+      const profile = await ExperienceSchema.find({username:username})
+      if (profile) {
+        res.send(profile)
+      } else {
+        const error = new Error()
+        error.httpStatusCode = 404
+        next(error)
+      }
+    } catch (error) {
+      console.log(error)
+      next("While reading experiences list a problem occurred!")
+    }
+  })
+
+
+  router.post("/:username/experiences/:expId/picture", upload.any("picture"), async (req, res, next) => {
+    console.log("mama")
+    console.log(req.files)
+    try {
+      if (req.params.username==auth(req).name){
+        await writeFile(
+          path.join(profilesFolderPath, req.files[0].originalname),
+          req.files[0].buffer
+        )
+        const user = await ExperienceSchema.findOneAndUpdate({ _id: req.params.expId }, {image:"file:///C:/Users/Dell/Documents/GitHub/linkedin-backend/src/public/"+(req.files[0].originalname)})
+        console.log(user)
+      }
+      if (user) {
+        res.send("Ok")
+      } else {
+        const error = new Error(`User with username ${auth(req).name} not found`)
+        error.httpStatusCode = 404
+        next(error)
+      }
+    } catch (error) {
+      console.log(error)
+      next(error)
+    }
+    res.send("ok")
+  })
+  
+
+  router.post(
+    "/:username/experiences",
+    [
+      check("role")
+        .isLength({ min: 2 }).withMessage("At least 2 characters")
+        .exists().withMessage("Insert a role please!"),
+        check("company")
+        .isLength({ min: 2 }).withMessage("At least 2 characters")
+        .exists().withMessage("Insert a company please!"),
+        check("startDate")
+        .isDate().withMessage("Please insert a valid date")
+        .exists().withMessage("Insert a date please!"),
+        check("description")
+        .isLength({ min: 2 }).withMessage("At least 2 characters")
+        .exists().withMessage("Insert a description please!"),
+        check("area")
+        .isLength({ min: 2 }).withMessage("At least 2 characters")
+        .exists().withMessage("Insert an area please!"),
+    ],
+    async (req, res, next) => {
+      try {
+        const errors = validationResult(req)
+      if (!errors.isEmpty()) {
+        let err = new Error()
+        err.message = errors
+        err.httpStatusCode = 400
+        next(err)
+      }
+      else{
+        const newProfile = {
+          ...req.body,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+          username: auth(req).name,
+        }
+        const newP2 = new ExperienceSchema(newProfile)
+        const { username } = await newP2.save()
+      
+        res.status(201).send(username)}
+      } catch (error) {
+        next(error)
+      }
+      })
+  
 
 module.exports = router
